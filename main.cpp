f27dfdbf7ef4ea615f58fc064341d0787ba77236
Angyal Ádám Szilárd <angeleb40@gmail.com>
2022-09-16 16:18:25 +0200

    Update main.cpp
    hibák leírása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //NELEMENTS-be alulvonás kell
+    std::cout << '1-100 ertekek duplazasa' //; hiányzik és nem '', hanem ""
+    for (int i = 0;) //pontatlan léptetési ciklus
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //nem 0-tól, hanem 1-től kell léptetni
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //pontatlan léptetési ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //értékek kiíratása hiányzik és a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //kezdőérték megadása hiányzik
+    for (int i = 0; i < N_ELEMENTS, i++) //; kell , helyett
     {
-        atlag += b[i]
+        atlag += b[i] //; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
