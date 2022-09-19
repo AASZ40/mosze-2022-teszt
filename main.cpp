0bb4743cedcd8099b3848bb22443cbb07fd571bb
Angyal Adam Szilard <angeleb40@gmail.com>
2022-09-19 10:35:45 +0200

    Update main.cpp
    Hibak javitva.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS-be alulvonás kell
-    std::cout << '1-100 ertekek duplazasa' //; hiányzik és nem '', hanem ""
-    for (int i = 0;) //pontatlan léptetési ciklus
+    int *b = new int[N_ELEMENTS]; //NELEMENTS-be alulvonás kell
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //; hiányzik és nem '', hanem ""
+    for (int i = 0; i < N_ELEMENTS; i++) //pontatlan léptetési ciklus
     {
-        b[i] = i * 2; //nem 0-tól, hanem 1-től kell léptetni
+        b[i] = (i + 1) * 2; //nem 0-tól, hanem 1-től kell léptetni
     }
-    for (int i = 0; i; i++) //pontatlan léptetési ciklus
+    for (int i = 0; i < N_ELEMENTS; i++) //pontatlan léptetési ciklus
     {
-        std::cout << "Ertek:" //értékek kiíratása hiányzik és a ;
+        std::cout << "Ertek:" << b[i] << std::endl; //értékek kiíratása hiányzik és a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //kezdőérték megadása hiányzik
-    for (int i = 0; i < N_ELEMENTS, i++) //; kell , helyett
+    int atlag = 0;; //kezdőérték megadása hiányzik
+    for (int i = 0; i < N_ELEMENTS; i++) //; kell , helyett
     {
-        atlag += b[i] //; hiányzik
+        atlag += b[i]; //; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
